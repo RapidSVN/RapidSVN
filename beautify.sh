a5ea0b2223dfc1e26329a75ebc2960325c8c33de
Alexander Mueller <xelarellum@tigris.org>
2002-07-31 21:22:10 +0000

    Added svn:executable property .orig files get deleted after call
    * tools/beautify.sh
    
    
    git-svn-id: http://rapidsvn.tigris.org/svn/rapidsvn/trunk/tools@2832 65390229-12b7-0310-b90b-f21a5aa7ec8e

--- beautify.sh
+++ beautify.sh
@@ -1,5 +1,8 @@
 #/bin/bash
 #Script to enforce rapidsvn coding styles
+#
+#Usage example to beautify all of the cpp files in src:
+#>for FILE in *.cpp ; do ../tools/beautify.sh $FILE ; done
 
 INDENT="indent --indent-level2 --braces-after-if-line --brace-indent0 --no-blank-lines-after-commas -nut --declaration-indentation1"
 FILE=$1
@@ -9,6 +12,7 @@ else
   mv $FILE $FILE.orig
   #remove tabs
   tr "\t" " " < $FILE.orig > $FILE
+  rm $FILE.orig
   #no for the style
   $INDENT $FILE
 fi
