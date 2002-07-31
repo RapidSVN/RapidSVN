0fa4003bca6e52af5b1e676a19ad464bab56b02d
Alexander Mueller <xelarellum@tigris.org>
2002-07-31 21:45:47 +0000

    Allow multiple targets.
    * tools/beautify.sh
    
    
    
    git-svn-id: http://rapidsvn.tigris.org/svn/rapidsvn/trunk/tools@2834 65390229-12b7-0310-b90b-f21a5aa7ec8e

--- beautify.sh
+++ beautify.sh
@@ -2,18 +2,19 @@
 #Script to enforce rapidsvn coding styles
 #
 #Usage example to beautify all of the cpp files in src:
-#>for FILE in *.cpp ; do ../tools/beautify.sh $FILE ; done
+#>../tools/beautify.sh *.cpp
 
 INDENT="indent --indent-level2 --braces-after-if-line --brace-indent0 --no-blank-lines-after-commas -nut --declaration-indentation1"
-FILE=$1
-if test ! -f $FILE ; then
-  echo "Invalid file $FILE" >&2
-else
-  mv $FILE $FILE.orig
-  #remove tabs
-  tr "\t" " " < $FILE.orig > $FILE
-  rm $FILE.orig
-  #no for the style
-  $INDENT $FILE
-fi
+for FILE in $@ ; do 
+  if test ! -f $FILE ; then
+    echo "Invalid file $FILE" >&2
+  else
+    mv $FILE $FILE.orig
+    #remove tabs
+    tr "\t" " " < $FILE.orig > $FILE.trout
+    #no for the style
+    $INDENT $FILE.trout -o $FILE
+    rm $FILE.trout
+  fi
+done
 
