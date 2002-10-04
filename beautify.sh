235be6c6ad693c57578682daf9aaaaa88638c1c2
Alexander Mueller <xelarellum@tigris.org>
2002-10-04 21:15:53 +0000

    Bugfix. Put the "-nut" arg first place, otherwise it seems to be ignored.
    git-svn-id: http://rapidsvn.tigris.org/svn/rapidsvn/trunk/tools@3285 65390229-12b7-0310-b90b-f21a5aa7ec8e

--- beautify.sh
+++ beautify.sh
@@ -4,7 +4,7 @@
 #Usage example to beautify all of the cpp files in src:
 #>../tools/beautify.sh *.cpp
 
-INDENT="indent --indent-level2 --braces-after-if-line --brace-indent0 --no-blank-lines-after-commas -nut --declaration-indentation1"
+INDENT="indent -nut --indent-level2 --braces-after-if-line --brace-indent0 --no-blank-lines-after-commas --declaration-indentation1"
 for FILE in $@ ; do 
   if test ! -f $FILE ; then
     echo "Invalid file $FILE" >&2
