def4761cf9a0464eb3be2cea3b05fd2cd9cc123f
Alexander Mueller <xelarellum@tigris.org>
2004-12-30 12:42:56 +0000

    New version 0.7.2
    git-svn-id: http://rapidsvn.tigris.org/svn/rapidsvn/tags/0.7.2@7134 65390229-12b7-0310-b90b-f21a5aa7ec8e

--- src/version.hpp
+++ src/version.hpp
@@ -37,11 +37,11 @@
 
 /* Patch number.
    Modify for every released patch. */
-#define RAPIDSVN_VER_MICRO 1
+#define RAPIDSVN_VER_MICRO 2
 
 /* Symbolic name of the milestone
    Modify whenever Minor number is changed. */
-#define RAPIDSVN_VER_MILESTONE "0.7.1"
+#define RAPIDSVN_VER_MILESTONE "0.7.2"
 
 #endif
 /* -----------------------------------------------------------------
