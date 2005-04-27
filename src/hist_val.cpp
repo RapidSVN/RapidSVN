ad6417bd6df9e0a68e7de4935f4bd49d7851719a
Alexander Mueller <xelarellum@tigris.org>
2005-04-27 16:44:11 +0000

    Added new source for a historical validator
    * src/hist_val.hpp, src/hist_val.cpp: added new files (with default contents)
    
    git-svn-id: http://rapidsvn.tigris.org/svn/rapidsvn/trunk@7158 65390229-12b7-0310-b90b-f21a5aa7ec8e

---
+++ src/hist_val.cpp
@@ -0,0 +1,22 @@
+/*
+ * ====================================================================
+ * Copyright (c) 2002-2004 The RapidSvn Group.  All rights reserved.
+ *
+ * This software is licensed as described in the file LICENSE.txt,
+ * which you should have received as part of this distribution.
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals.  For exact contribution history, see the revision
+ * history and logs, available at http://rapidsvn.tigris.org/.
+ * ====================================================================
+ */
+
+// app
+#include "hist_val.hpp"
+
+
+/* -----------------------------------------------------------------
+ * local variables:
+ * eval: (load-file "../rapidsvn-dev.el")
+ * end:
+ */
