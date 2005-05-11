d226abfbfa70509b50d5405dc39177954bc990c8
Alexander Mueller <xelarellum@tigris.org>
2005-05-11 05:14:47 +0000

    Issue #278: autogen.sh on OSX needs 'glibtoolize' instead of 'libtoolize'
    * autogen.sh: try to run "glibtoolize" if "libtoolize" fails (=isnt there)
    
    git-svn-id: http://rapidsvn.tigris.org/svn/rapidsvn/trunk@7170 65390229-12b7-0310-b90b-f21a5aa7ec8e

--- autogen.sh
+++ autogen.sh
@@ -2,7 +2,10 @@
 
 aclocal
 autoconf
-libtoolize --automake --force
+# on some platforms "libtoolize" is "glibtoolize"
+# like on Mac OS/X. In this case, if "libtoolize" fails
+# run "glibtoolize"
+libtoolize --automake --force || glibtoolize --automake --force
 aclocal
 automake -a
 
