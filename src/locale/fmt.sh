598fd829cdfcd142ddf1bf3b8c61b91c3d86f460
Alexander Mueller <xelarellum@tigris.org>
2006-10-28 16:59:48 +0000

    Issue 453: Update translations Patch contributed by tommaso massimi: Italian translation for rapisvn (with some minor modifications caused by errors in the translated file)
    * src/locale/it_IT: added
    * src/locale/fmt.sh: updated
    * src/locale/update.sh
    
    git-svn-id: http://rapidsvn.tigris.org/svn/rapidsvn/trunk@7570 65390229-12b7-0310-b90b-f21a5aa7ec8e

--- src/locale/fmt.sh
+++ src/locale/fmt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-for X in de fr pt_BR ru uk zh_CN ; do
+for X in de fr it_IT pt_BR ru uk zh_CN ; do
 	msgfmt -o "$X/rapidsvn.mo" "$X/rapidsvn.po"
 done
--- src/locale/update.sh
+++ src/locale/update.sh
@@ -5,7 +5,7 @@
 # in the language subdirectories
 
 xgettext -C -k_ -s -o rapidsvn.pot ../*.cpp
-for X in de fr pt_BR ru uk zh_CN ; do
+for X in de fr it_IT pt_BR ru uk zh_CN ; do
 	msgmerge -U $X/rapidsvn.po rapidsvn.pot
 done
 
