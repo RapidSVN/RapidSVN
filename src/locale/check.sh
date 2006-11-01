900d0ea663d04d0050f0db7f5327b1c2f1e1cb22
Alexander Mueller <xelarellum@tigris.org>
2006-11-01 20:59:23 +0000

    Issue 453: Update translations
    * src/locale/check.sh: modified the script, so only the names of
      the translations, that have to be checked, are displayed
    
    git-svn-id: http://rapidsvn.tigris.org/svn/rapidsvn/trunk@7576 65390229-12b7-0310-b90b-f21a5aa7ec8e

--- src/locale/check.sh
+++ src/locale/check.sh
@@ -3,5 +3,5 @@
 # This script which of the translations contain "fuzzy" strings
 # that have to be checked and corrected by the translators
 echo "These files have to be checked:"
-find -name rapidsvn.po -exec grep -l fuzzy {} \;
+find -name rapidsvn.po -exec grep -l fuzzy {} \; | sed -e "s/^.\///" -e "s/\/.*$//" | sort
 
