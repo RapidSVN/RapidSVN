35d68f4c84137f7abe16ccb4b039ffdfd92196a1
Alexander Mueller <xelarellum@tigris.org>
2009-07-23 07:10:32 +0000

    Adapted the InnoSetup file to the Subversion binary developer release directory structure.
    git-svn-id: http://rapidsvn.tigris.org/svn/rapidsvn/branches/0.10-1@7994 65390229-12b7-0310-b90b-f21a5aa7ec8e

--- packages/win32/FetchFiles.bat
+++ packages/win32/FetchFiles.bat
@@ -20,14 +20,9 @@ del tmp\*.so
 rem Copy Berkeley DB
 copy %SUBVERSION%\db4-win32\bin\libdb44.dll tmp
 
-rem Copy APR DLLs
-copy %SUBVERSION%\apr\Release\libapr-1.dll tmp
-copy %SUBVERSION%\apr-util\Release\libaprutil-1.dll tmp
-copy %SUBVERSION%\apr-iconv\Release\libapriconv-1.dll tmp
-copy %SUBVERSION%\apr-iconv\Release\iconv\*.so tmp
-
-rem Copy Libintl DLL
-copy %LIBINTL%\bin\intl3_svn.dll tmp
+rem Copy Subversion and APR DLLs
+copy %SUBVERSION%\bin\*.dll tmp
+copy %SUBVERSION%\iconv\*.so tmp
 
 rem Copy C++ Runtime
 copy msvcp60.dll tmp
