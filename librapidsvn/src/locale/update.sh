71fcdf365d6c16494a19be2c352d0d6fe65b8629
Florian Fischer <florianfischer@gmx.de>
2017-12-29 21:56:21 +0100

    Updated German translation.
    Fix locale-update script to find all relevant strings.

--- librapidsvn/src/locale/update.sh
+++ librapidsvn/src/locale/update.sh
@@ -4,7 +4,7 @@
 # and merges it with the existing translations
 # in the language subdirectories
 
-xgettext -C -k_ -s -o rapidsvn.pot ../*.cpp
+xgettext -C -k_ -s -o rapidsvn.pot ../*.cpp ../../include/*.h ../../../rapidsvn/*.cpp
 for X in de fr hu it_IT pt_BR ru es uk zh_CN ja ; do
 	msgmerge -U $X/rapidsvn.po rapidsvn.pot
 done
