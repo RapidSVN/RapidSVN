2d8663f996f30bed7062f76d77a75fd4caad91f2
Alexander Mueller <xelarellum@web.de>
2018-03-19 22:30:31 +0100

    Change libsvncpp to LGPLv3

--- libsvncpp/src/info.cpp
+++ libsvncpp/src/info.cpp
@@ -1,11 +1,11 @@
 /*
  * ====================================================================
- * Copyright (c) 2002-2012 The RapidSVN Group.  All rights reserved.
+ * Copyright (c) 2002-2018 The RapidSVN Group.  All rights reserved.
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
